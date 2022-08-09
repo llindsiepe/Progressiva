2e2c7f0f2aac990b49e014f5b3fb321f111590af
Lucas Salles Souza <lucas.souza@saber.parceirosedu.com.br>
2022-08-09 19:52:47 -0300

    header

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,6 +4,7 @@ import profile from "../../imagens/profile.png";
 import { Main, LogotipoImage, Menu, Entrar, OptionEntrar } from "./styles.js";
 import OptionMenu from "../OptionMenu/index.js";
 import Button from "../Button/index.js";
+import Grid from "@mui/material/Grid";
 
 import { useHistory, useLocation } from "react-router-dom";
 
@@ -14,57 +15,67 @@ export default function Navbar() {
   return (
     <>
       <Main>
-        <LogotipoImage>
-          <img src={Logotipo} width="152" height="61" />
-        </LogotipoImage>
-
-        <Menu style={{ display: "flex" }}>
-          <OptionMenu
-            active={location.pathname == "/"}
-            click={() => {
-              navigate.push("/");
-            }}
-            title={"Home"}
-          />
-          <OptionMenu
-            active={location.pathname == "/sobre"}
-            click={() => {
-              navigate.push("/sobre");
-            }}
-            title={"Sobre"}
-          />
-          <OptionMenu
-            active={location.pathname == "/infraestrutura"}
-            click={() => {
-              navigate.push("/infraestrutura");
-            }}
-            title={"Infraestrutura"}
-          />
-          <OptionMenu
-            active={location.pathname == "/contato"}
-            click={() => {
-              navigate.push("/contato");
-            }}
-            title={"Contato"}
-          />
-        </Menu>
-
-        <Entrar>
-          <a
-            href="https://progressivatransportes.eslcloud.com.br/guests/sign_in"
-            target="_blank"
-          >
-            <OptionEntrar>
-              <img width="24" height="24" src={profile} /> ENTRAR
-            </OptionEntrar>
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=5511940305742"
-            target="_blank"
-          >
-            <Button title={"COTAÇÃO ONLINE"} />
-          </a>
-        </Entrar>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="space-around"
+        >
+          <Grid item>
+            <LogotipoImage>
+              <img src={Logotipo} width="152" height="61" />
+            </LogotipoImage>
+          </Grid>
+          <Grid item>
+            <Menu style={{ display: "flex", marginTop: 20 }}>
+              <OptionMenu
+                active={location.pathname == "/"}
+                click={() => {
+                  navigate.push("/");
+                }}
+                title={"Home"}
+              />
+              <OptionMenu
+                active={location.pathname == "/sobre"}
+                click={() => {
+                  navigate.push("/sobre");
+                }}
+                title={"Sobre"}
+              />
+              <OptionMenu
+                active={location.pathname == "/infraestrutura"}
+                click={() => {
+                  navigate.push("/infraestrutura");
+                }}
+                title={"Infraestrutura"}
+              />
+              <OptionMenu
+                active={location.pathname == "/contato"}
+                click={() => {
+                  navigate.push("/contato");
+                }}
+                title={"Contato"}
+              />
+            </Menu>
+          </Grid>
+          <Grid item>
+            <Entrar>
+              <a
+                href="https://progressivatransportes.eslcloud.com.br/guests/sign_in"
+                target="_blank"
+              >
+                <OptionEntrar>
+                  <img width="24" height="24" src={profile} /> ENTRAR
+                </OptionEntrar>
+              </a>
+              <a
+                href="https://api.whatsapp.com/send?phone=5511940305742"
+                target="_blank"
+              >
+                <Button title={"COTAÇÃO ONLINE"} />
+              </a>
+            </Entrar>
+          </Grid>
+        </Grid>
       </Main>
     </>
   );
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -2,37 +2,19 @@ import styled from "styled-components";
 
 export const Main = styled.div`
   width: 100%;
-  height: 15%;
   background-color: white;
   box-shadow: 0px 0px 20px #e5e5e5;
-  display: flex;
-  justify-content: space-between;
-  @media (max-width: 768px) {
-    display: block;
-    padding: 15px;
-  }
+  padding: 5px;
 `;
 export const LogotipoImage = styled.div`
   margin-top: auto;
   margin-bottom: auto;
   margin-left: 10%;
-  @media (max-width: 768px) {
-    img{
-    }
-  }
-  @media (max-width: 450px) {
-    margin-left: 50%;
-    transform: translate(-40%)
-  }
+  
 `;
 export const Menu = styled.div`
   gap: 2rem;
-  @media (max-width: 768px) {
-    margin-left: auto;
-    margin-top: 25px;
-  }
-  @media (max-width: 450px) {
-  }
+  
 `;
 
 
@@ -41,22 +23,10 @@ export const Entrar = styled.div`
   text-align: center;
   align-items: center;
   display: flex;
-  margin-top: auto;
+  margin-top: 5px;
   margin-bottom: auto;
   gap: 1rem;
   margin-right: 10%;
-  @media (max-width: 768px) {
-    margin-left: 50%;
-    transform: translate(-45%);
-    margin-top: 25px;
-    img {
-       display: none;
-    }
-  }
-
-  @media (max-width: 450px) {
-    margin-left: 35%
-  }
 `;
 
 export const OptionEntrar = styled.button`
