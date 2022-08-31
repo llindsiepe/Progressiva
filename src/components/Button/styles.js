9d8bd3e7c0c36cb166b5966c86b0b667ebfa295c
Lucas756 <salleslucas00@gmail.com>
2022-08-31 13:52:18 -0300

    oi

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,19 +1,11 @@
 import styled from "styled-components";
 
 export const Option = styled.button`
-  width: 215px;
+  
   height: 40px;
   border-radius: 5px;
   background-color: #00923f;
   color: white;
   border: 1px solid white;
-
-  @media (max-width: 768px) {
-    height: 30px;
-    width: 120px;
-  }
-  @media (max-width: 451px) {
-    height: 30px;
-    width: 50px;
-  }
+  padding: 1px;
 `;
